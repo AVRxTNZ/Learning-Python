a3afccac322a0b06cb95fa6f4a5246ee16d78244
Tanza <99956763+AVRxTNZ@users.noreply.github.com>
2022-02-22 11:43:04 +0800

    First Program with Python

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+#Input
+print("Hello World")
+
+name ="AVRxTNZ"
+price = 10
+disc = "25%"
+
+print("My name is " + name)
+print("I bought a shirt yesterday for $" + str(price) + "with a " + disc + "discount") #For show the "price" with other we must using "str" to convert price to string
+
+'''
+--- Output ---
+Hello World
+My name is AVRxTNZ
+I bought a shirt yesterday for $10 with a 25% discount
+'''
