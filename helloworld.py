3bee7ec8970f7d834fee4034fd8a1822666a816f
Tanza <99956763+AVRxTNZ@users.noreply.github.com>
2022-02-23 23:53:49 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -8,6 +8,13 @@ disc = "25%"
 print("My name is " + name)
 print("I bought a shirt yesterday for $" + str(price) + "with a " + disc + "discount") #For show the "price" with other we must using "str" to convert price to string
 
+
+function tambah(angka1, angka2):
+  return angka1 + angka2
+
+tambah()
+
+
 '''
 --- Output ---
 Hello World
