a4a5331f0f0cbe79cdc672d2af43ee900587eaf8
Tanza <99956763+AVRxTNZ@users.noreply.github.com>
2022-03-02 01:25:38 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -9,10 +9,17 @@ print("My name is " + name)
 print("I bought a shirt yesterday for $" + str(price) + "with a " + disc + "discount") #For show the "price" with other we must using "str" to convert price to string
 
 
-function tambah(angka1, angka2):
+#Fungsi
+def tambah(angka1, angka2):
+  hasil = angka1, angka2
   return angka1 + angka2
 
-tambah()
+print (tambah(5, 10))
+
+def kurang(kurang1, kurang2):
+  return kurang1 - kurang2
+
+print(kurang(25, 15)
 
 
 '''
@@ -20,4 +27,6 @@ tambah()
 Hello World
 My name is AVRxTNZ
 I bought a shirt yesterday for $10 with a 25% discount
+15
+10
 '''
